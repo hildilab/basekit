602b762d3e85c2c9430edf6fe744227e117f0325
arose <arose@arose>
2013-06-04 19:40:47 +0200

    ADD: DefaultOrderedDict class FIX: dssp cmd

--- basekit/utils/__init__.py
+++ basekit/utils/__init__.py
@@ -5,6 +5,7 @@ from __future__ import with_statement
 import collections
 import itertools
 import contextlib
+import copy
 import os
 import re
 
@@ -125,3 +126,42 @@ def copy_dict( dict, **kwargs ):
     dict2 = dict.copy()
     dict2.update( kwargs )
     return dict2
+
+
+
+
+class DefaultOrderedDict( collections.OrderedDict ):
+    def __init__(self, default_factory=None, *a, **kw):
+        if (default_factory!=None and not callable(default_factory)):
+            raise TypeError('first argument must be callable')
+        collections.OrderedDict.__init__(self, *a, **kw)
+        self.default_factory = default_factory
+    def __getitem__(self, key):
+        try:
+            return collections.OrderedDict.__getitem__(self, key)
+        except KeyError:
+            return self.__missing__(key)
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+    def __reduce__(self):
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+    def copy(self):
+        return self.__copy__()
+    def __copy__(self):
+        return type(self)(self.default_factory, self)
+    def __deepcopy__(self, memo):
+        return type(self)(
+            self.default_factory, copy.deepcopy(self.items())
+        )
+    def __repr__(self):
+        return 'OrderedDefaultDict(%s, %s)' % (
+            self.default_factory, collections.OrderedDict.__repr__(self)
+        )
+
