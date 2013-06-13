dd9c68166c19fb4adce684480714e2f3aac11307
arose <arose@arose>
2013-06-13 18:40:49 +0200

    ADD: more utils.path functions

--- basekit/utils/__init__.py
+++ basekit/utils/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import division
 from __future__ import with_statement
 
-
 import collections
 import itertools
 import contextlib
@@ -9,6 +8,8 @@ import copy
 import os
 import re
 
+import job
+import path
 
 
 def memoize(f):
--- basekit/utils/path.py
+++ basekit/utils/path.py
@@ -1,26 +1,33 @@
 
 
 import os
-import string
+from os.path import basename, splitext, join, dirname
 
 
+def ext( file_name ):
+    return splitext( file_name )[1]
 
 def stem( file_name ):
-    return os.path.splitext( os.path.split( file_name )[-1] )[0]
+    return splitext( basename( file_name ) )[0]
 
 
-def tmpl( template, file_name, directory=None ):
-    values = { 
-    	"stem": stem( file_name )
-	}
-    new_name = string.Template( template ).substitute( **values )
-    if directory:
-        new_name = os.path.join( directory, new_name )
-    return new_name
+def add_prefix( file_name, prefix="" ):
+	return join( 
+		dirname( file_name ), 
+		prefix + basename( file_name ) 
+	)
 
+def add_suffix( file_name, suffix="" ):
+	return join( 
+        dirname( file_name ),
+        stem( file_name ) + suffix + ext( filename )
+    )
 
-def new_ext( file_name, new_ext, directory=None ):
-    return tmpl( "${stem}." + new_ext )
+def change_ext( file_name, extension="" ):
+	return join( 
+        dirname( file_name ),
+        stem( file_name ) + "." + extension
+    )
 
 
 
