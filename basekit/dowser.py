99b52cc28e4ce7c5ed83d3e5f125fac1f3d05fa7
arose <arose@arose>
2013-06-19 15:38:45 +0200

    ADD: dowser intsurf.pdb shown in provi ADD: dowser dna, rna params (untested) CHANGE: dowserx can be used in conjunction with dowser-repeat

--- basekit/dowser.py
+++ basekit/dowser.py
@@ -1,3 +1,9 @@
+"""
+    http://danger.med.unc.edu/hermans/dowser/Dowman.htm
+    http://www.ks.uiuc.edu/Research/vmd/plugins/dowser/
+    http://muralab.org/~cmura/DOWSER/
+    http://www.ks.uiuc.edu/Research/vmd/plugins/dowser/dowser-rna.html
+"""
 
 import os
 import shutil
@@ -12,6 +18,8 @@ DOWSER_CMD = "dowser"
 DOWSERX_CMD = "dowserx"
 DOWSER_REPEAT_CMD = "dowser-repeat"
 
+DOWSER_ATOMDICT = os.path.join( TMPL_DIR, "atomdict.db" )
+DOWSER_ATOMPARMS = os.path.join( TMPL_DIR, "atomparms.db" )
 
 DOWSER_ARGS = [
     _( "pdb_file", type="file", ext="pdb" ),
@@ -20,13 +28,14 @@ DOWSER_ARGS = [
     _( "onlyxtalwater", type="checkbox", default=False ),
     _( "probe", type="float", default=0.2 ),
     _( "separation", type="float", default=1.0 ),
-    _( "atomtypes", type="text", default=False ),
-    _( "atomparms", type="text", default=False )
+    _( "atomtypes", type="text", default=DOWSER_ATOMDICT ),
+    _( "atomparms", type="text", default=DOWSER_ATOMPARMS )
 ]
 DOWSER_OUT = [
     _( "input_file", file="myinput.pdb" ),
     _( "wat_file", file="dowserwat.pdb" ),
-    _( "watall_file", file="dowserwat_all.pdb" )
+    _( "watall_file", file="dowserwat_all.pdb" ),
+    _( "intsurf_file", file="intsurf.pdb" )
 ]
 
 class Dowser( CmdTool, ProviMixin ):
@@ -47,7 +56,7 @@ class Dowser( CmdTool, ProviMixin ):
         self.cmd = [ 
             exe, self.relpath( self.input_file ), 
             "-probe", self.probe,
-            "separation", self.separation
+            "-separation", self.separation
         ]
         if self.hetero: self.cmd.append( "-hetero" )
         if self.noxtalwater: self.cmd.append( "-noxtalwater" )
@@ -60,7 +69,8 @@ class Dowser( CmdTool, ProviMixin ):
         self._make_provi_file(
             input_file=self.relpath( self.input_file ),
             wat_file=self.relpath( self.wat_file ),
-            watall_file=self.relpath( self.watall_file )
+            watall_file=self.relpath( self.watall_file ),
+            intsurf_file=self.relpath( self.intsurf_file )
         )
 
 
@@ -68,7 +78,9 @@ class DowserRepeat( PyTool, ProviMixin ):
     """ A wrapper around the 'dowser-repeat' programm, which is
         called until no more new waters are found.
     """
-    args = DOWSER_ARGS
+    args = DOWSER_ARGS + [
+        _( "alt", type="select", options=["x"], default=None )
+    ]
     out = DOWSER_OUT + [
         _( "repeat_dir", dir="repeats" )
     ]
@@ -82,7 +94,7 @@ class DowserRepeat( PyTool, ProviMixin ):
         dowserwat = []
         dowserwat_all = []
         while True:
-            alt = "repeat" if rep_count else None
+            alt = "repeat" if rep_count else self.alt
             rep_out = os.path.join( self.repeat_dir, str(rep_count) )
             dowser = Dowser( self.pdb_file, **copy_dict( 
                 self.kwargs, run=False, output_dir=rep_out, alt=alt
@@ -111,12 +123,15 @@ class DowserRepeat( PyTool, ProviMixin ):
             fp.writelines( dowserwat )
         with open( self.watall_file, "w" ) as fp:
             fp.writelines( dowserwat_all )
+        # copy intsurf
+        shutil.copy( dowser.intsurf_file, self.intsurf_file )
     def _pre_exec( self ):
         shutil.copy( self.pdb_file, self.input_file )
     def _post_exec( self ):
         self._make_provi_file(
             input_file=self.relpath( self.input_file ),
             wat_file=self.relpath( self.wat_file ),
-            watall_file=self.relpath( self.watall_file )
+            watall_file=self.relpath( self.watall_file ),
+            intsurf_file=self.relpath( self.intsurf_file )
         )
 
