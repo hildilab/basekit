349f0337e4cb2a27fd2e77669795b4ea96bb4ec1
arose <arose@arose>
2013-06-27 19:40:10 +0200

    ADD: cython align code

---
+++ basekit/utils/align/__init__.py
@@ -0,0 +1,4 @@
+try: 
+    from calign import aligner, score_alignment
+except ImportError:
+    from align import aligner
--- setup.py
+++ setup.py
@@ -2,28 +2,36 @@ from setuptools import setup, Extension
 #from distutils.core import setup
 #from distutils.extension import Extension
 
-# try:
-#     from Cython.Distutils import build_ext
-#     cmdclass = { 'build_ext': build_ext }
-# except ImportError:
-#     cmdclass = {}
-# print cmdclass
-# import numpy
-# np_include = numpy.get_include()
+try:
+    from Cython.Distutils import build_ext
+    cmdclass = { 'build_ext': build_ext }
+except ImportError:
+    cmdclass = {}
+
+import numpy
+np_include = numpy.get_include()
 
 setup(
     name = 'basekit',
     version = '0.0.2',
-    #cmdclass = cmdclass,
+    cmdclass = cmdclass,
     ext_modules = [ 
         Extension(
             "basekit/utils/cgeom", 
             sources = [ "basekit/utils/cgeom.c" ]
+        ),
+        Extension(
+            "basekit/utils/align/calign", 
+            sources = [ "basekit/utils/align/calign.c" ]
         )
     ],
     url = 'www.weirdbyte.de',
     author = 'Alexander Rose',
     author_email = 'alexander.rose@weirdbyte.de',
-    packages = [ 'basekit', 'basekit.utils' ],
+    packages = [ 
+        'basekit', 
+        'basekit.utils',
+        'basekit.utils.align'
+    ],
     install_requires = [ 'numpy', 'matplotlib' ]
 )
