596bff4e4fd3180c4d3fa217f40d18db5c136afb
arose <arose@arose>
2013-07-26 16:53:41 +0200

    CHANGE: dowser tool: doeser.pdb with all waters+input pdb CHANGE: numpdb:         - more sele kwargs         - write2 method         - read hetatm

--- basekit/dowser.py
+++ basekit/dowser.py
@@ -11,7 +11,8 @@ import shutil
 
 from utils import copy_dict
 from utils.tool import _, _dir_init, CmdTool, PyTool, ProviMixin
-from utils.job import run_command
+from utils.numpdb import NumPdb
+
 
 DIR, PARENT_DIR, TMPL_DIR = _dir_init( __file__, "dowser" )
 
@@ -21,6 +22,38 @@ DOWSER_REPEAT_CMD = "dowser-repeat"
 
 DOWSER_ATOMDICT = os.path.join( TMPL_DIR, "atomdict.db" )
 DOWSER_ATOMPARMS = os.path.join( TMPL_DIR, "atomparms.db" )
+
+
+class DowserMixin( object ):
+    def _pre_exec( self ):
+        shutil.copy( self.pdb_file, self.input_file )
+    def _post_exec( self ):
+        self._make_provi_file(
+            input_file=self.relpath( self.input_file ),
+            wat_file=self.relpath( self.wat_file ),
+            watall_file=self.relpath( self.watall_file ),
+            intsurf_file=self.relpath( self.intsurf_file )
+        )
+        # write a pdb with all dowser waters but no others
+        with open( self.dowser_file, "w" ) as fp:
+            with open( self.pdb_file, "r" ) as fp_pdb:
+                for line in fp_pdb:
+                    if ( line[0:6] in ["ATOM  ", "HETATM"] and
+                            line[17:20]!="HOH" ):
+                        fp.write( line )
+            with open( self.wat_file, "r" ) as fp_wat:
+                for line in fp_wat:
+                    if line[0:6] in ["HETATM"]:
+                        fp.write( line )
+        # rename HOH.OW atoms to HOH.O and
+        # repair atom index
+        npdb = NumPdb( self.dowser_file )
+        for i, a in enumerate( npdb._atoms, start=1 ):
+            if a["atomname"]==" OW " and a["resname"]=="HOH":
+                a["atomname"] = " O  "
+            a["atomno"] = i
+        npdb.write( self.dowser_file )
+
 
 DOWSER_ARGS = [
     _( "pdb_file", type="file", ext="pdb" ),
@@ -36,10 +69,11 @@ DOWSER_OUT = [
     _( "input_file", file="myinput.pdb" ),
     _( "wat_file", file="dowserwat.pdb" ),
     _( "watall_file", file="dowserwat_all.pdb" ),
-    _( "intsurf_file", file="intsurf.pdb" )
+    _( "intsurf_file", file="intsurf.pdb" ),
+    _( "dowser_file", file="dowser.pdb" )
 ]
 
-class Dowser( CmdTool, ProviMixin ):
+class Dowser( DowserMixin, CmdTool, ProviMixin ):
     """ A wrapper around the 'dowser' programm. """
     args = DOWSER_ARGS + [
         _( "alt", type="select", options=["x", "repeat"], default=None )
@@ -64,18 +98,9 @@ class Dowser( CmdTool, ProviMixin ):
         if self.onlyxtalwater: self.cmd.append( "-onlyxtalwater" )
         if self.atomtypes: self.cmd += [ "-atomtypes", self.atomtypes ]
         if self.atomparms: self.cmd += [ "-atomparms", self.atomparms ]
-    def _pre_exec( self ):
-        shutil.copy( self.pdb_file, self.input_file )
-    def _post_exec( self ):
-        self._make_provi_file(
-            input_file=self.relpath( self.input_file ),
-            wat_file=self.relpath( self.wat_file ),
-            watall_file=self.relpath( self.watall_file ),
-            intsurf_file=self.relpath( self.intsurf_file )
-        )
+    
 
-
-class DowserRepeat( PyTool, ProviMixin ):
+class DowserRepeat( DowserMixin, PyTool, ProviMixin ):
     """ A wrapper around the 'dowser-repeat' programm, which is
         called until no more new waters are found.
     """
@@ -94,6 +119,7 @@ class DowserRepeat( PyTool, ProviMixin ):
         rep_count = 0
         dowserwat = []
         dowserwat_all = []
+        max_resno = 0
         while True:
             alt = "repeat" if rep_count else self.alt
             rep_out = os.path.join( self.repeat_dir, str(rep_count) )
@@ -112,10 +138,24 @@ class DowserRepeat( PyTool, ProviMixin ):
                 new_wat = fp.readlines()
             with open( dowser.watall_file, "r" ) as fp:
                 new_watall = fp.readlines()
-            # checks if there are new waters
-            if len( new_wat ) > 1 and len( new_watall ) > 1:
-                dowserwat += new_wat
-                dowserwat_all += new_watall
+            # increment water resno
+            new_wat2 = []
+            for l in new_wat:
+                if l[0:6]=="HETATM":
+                    resno = int( l[22:26] ) + max_resno
+                    l = l[0:22] + "{:>4}".format( resno ) + l[26:]
+                new_wat2.append( l )
+            new_watall2 = []
+            for l in new_watall:
+                if l[0:6]=="HETATM":
+                    resno = int( l[22:26] ) + max_resno
+                    l = l[0:22] + "{:>4}".format( resno ) + l[26:]
+                new_watall2.append( l )
+            max_resno = max( map( lambda x: int(x[22:26]), new_watall2 ) )
+            # check if there are new waters
+            if len( new_wat2 ) > 1 and len( new_watall2 ) > 1:
+                dowserwat += new_wat2
+                dowserwat_all += new_watall2
                 rep_count += 1
             else:
                 break
@@ -126,13 +166,5 @@ class DowserRepeat( PyTool, ProviMixin ):
             fp.writelines( dowserwat_all )
         # copy intsurf
         shutil.copy( dowser.intsurf_file, self.intsurf_file )
-    def _pre_exec( self ):
-        shutil.copy( self.pdb_file, self.input_file )
-    def _post_exec( self ):
-        self._make_provi_file(
-            input_file=self.relpath( self.input_file ),
-            wat_file=self.relpath( self.wat_file ),
-            watall_file=self.relpath( self.watall_file ),
-            intsurf_file=self.relpath( self.intsurf_file )
-        )
+
 
